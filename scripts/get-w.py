78546fa1856444ea8407c5572194a818aac3fc14
mizdra <pp.mizdra@gmail.com>
2019-12-14 22:08:06 +0900

    ディレクトリ構成を変更

--- scripts/get-w.py
+++ scripts/get-w.py
@@ -4,7 +4,7 @@ import serial
 from time import sleep
 from datetime import date
 
-from controller import Controller
+from lib.controller import Controller
 
 # 1. 日付を2000/1/1に合わせる
 # 2. W回収済み & 柱のある祠の前へ移動
